4d3d580a1f63221caac16436984aa2b5e4753018
KingsDev <63374258+KingsMMA@users.noreply.github.com>
2024-12-13 19:06:20 +1000

    Added /warn

---
+++ src/discord/commands/private/warn.command.ts
@@ -0,0 +1,75 @@
+import type {AutocompleteInteraction, ChatInputCommandInteraction} from 'discord.js';
+import {PermissionsBitField} from 'discord.js';
+import {ApplicationCommandOptionType, ApplicationCommandType} from 'discord-api-types/v10';
+
+import type NetherixBot from '../../netherixBot';
+import KingsDevEmbedBuilder from '../../utils/kingsDevEmbedBuilder';
+import BaseCommand from '../base.command';
+import {Snowflake} from "discord-api-types/globals";
+
+export default class WarnCommand extends BaseCommand {
+    constructor(client: NetherixBot) {
+        super(client, {
+            name: 'warn',
+            description: 'Warns a user in the server.',
+            type: ApplicationCommandType.ChatInput,
+            default_member_permissions: PermissionsBitField.Flags.ModerateMembers.toString(),
+            options: [
+                {
+                    name: 'user',
+                    description: 'The user you want to warn.',
+                    type: ApplicationCommandOptionType.User,
+                    required: true,
+                },
+                {
+                    name: 'reason',
+                    description: 'The reason for warning the user.',
+                    type: ApplicationCommandOptionType.String,
+                    required: false,
+                }
+            ],
+        });
+    }
+
+    async execute(interaction: ChatInputCommandInteraction) {
+        await interaction.deferReply();
+
+        const user = interaction.options.getUser('user', true);
+        const member = interaction.guild!.members.cache.get(user.id);
+        if (!member)
+            return interaction.replyError('User not found.');
+
+        const reason = interaction.options.getString('reason') ?? 'No reason provided.';
+
+        await member.send({
+            embeds: [
+                new KingsDevEmbedBuilder()
+                    .setTitle('You have been warned.')
+                    .setColor('Red')
+                    .setDescription(`You have been warned in ${interaction.guild!.name}.`)
+                    .addField('Reason', reason)
+            ]
+        }).catch(() => {});
+
+        let userData = await this.client.main.mongo.getUserData(interaction.guildId!, user.id);
+        userData.cases.push({
+            user: user.id,
+            type: 'Warned',
+            reason: reason,
+            moderator: interaction.user.tag,
+            date: new Date(),
+        });
+        await this.client.main.mongo.saveUserData(userData);
+
+        return interaction.editReply({
+            embeds: [
+                new KingsDevEmbedBuilder()
+                    .setTitle('User Warned')
+                    .setColor('Red')
+                    .setDescription(`${user.tag} has been warned.`)
+                    .addField('Reason', reason)
+            ]
+        });
+    }
+
+}
